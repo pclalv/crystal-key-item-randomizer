30ceea30724b7050240981cadd4e75f2e984d717
paul alvarez <palvarez@greenhouse.io>
2017-10-17 09:38:06 -0400

    also update item_attributes

--- lib/crystal_key_item_randomizer.ex
+++ lib/crystal_key_item_randomizer.ex
@@ -85,33 +85,33 @@ defmodule CrystalKeyItemRandomizer do
   ]
 
   @item_attributes %{
-BASEMENT_KEY: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE"
-BICYCLE: => "	item_attribute     0, 0,                 0,               CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE"
-BLUE_CARD: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CURRENT, ITEMMENU_NOUSE"
-CARD_KEY: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE"
-CLEAR_BELL: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE"
-COIN_CASE: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CURRENT, ITEMMENU_NOUSE"
-GOOD_ROD: => "	item_attribute     0, 0,                 0,               CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE"
-HM_CUT: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE"
-HM_FLY: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE"
-HM_SURF: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE"
-HM_STRENGTH: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE"
-HM_FLASH: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE"
-HM_WHIRLPOOL: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE"
-HM_WATERFALL: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE"
-ITEMFINDER: => "	item_attribute     0, 0,                 0,               CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE"
-LOST_ITEM: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE"
-MACHINE_PART: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE"
-MYSTERY_EGG: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE"
-OLD_ROD: => "	item_attribute     0, 0,                 0,               CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE"
-PASS: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE"
-RED_SCALE: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE"
-S_S_TICKET: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE"
-SECRETPOTION: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE"
-SILVER_WING: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE"
-SUPER_ROD: => "	item_attribute     0, 0,                 0,               CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE"
-SQUIRTBOTTLE: => "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE"
-}
+    BASEMENT_KEY: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE",
+    BICYCLE: "	item_attribute     0, 0,                 0,               CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE",
+    BLUE_CARD: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CURRENT, ITEMMENU_NOUSE",
+    CARD_KEY: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE",
+    CLEAR_BELL: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE",
+    COIN_CASE: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CURRENT, ITEMMENU_NOUSE",
+    GOOD_ROD: "	item_attribute     0, 0,                 0,               CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE",
+    HM_CUT: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE",
+    HM_FLY: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE",
+    HM_SURF: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE",
+    HM_STRENGTH: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE",
+    HM_FLASH: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE",
+    HM_WHIRLPOOL: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE",
+    HM_WATERFALL: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, TM_HM,    ITEMMENU_PARTY,   ITEMMENU_NOUSE",
+    ITEMFINDER: "	item_attribute     0, 0,                 0,               CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE",
+    LOST_ITEM: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE",
+    MACHINE_PART: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE",
+    MYSTERY_EGG: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE",
+    OLD_ROD: "	item_attribute     0, 0,                 0,               CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE",
+    PASS: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE",
+    RED_SCALE: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE",
+    S_S_TICKET: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE",
+    SECRETPOTION: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE",
+    SILVER_WING: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_NOUSE,   ITEMMENU_NOUSE",
+    SUPER_ROD: "	item_attribute     0, 0,                 0,               CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE",
+    SQUIRTBOTTLE: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE",
+  }
 
   def required_items, do: @required_items
   def maybe_required_items, do: @maybe_required_items
@@ -138,10 +138,80 @@ SQUIRTBOTTLE: => "	item_attribute     0, 0,                 0, CANT_SELECT | CAN
     CrystalKeyItemRandomizer.key_items
     |> Enum.shuffle
     |> Enum.zip(CrystalKeyItemRandomizer.key_items)
-|> Enum.into(%{})
-    # CrystalKeyItemRandomizer.sudowoodo_locked_swaps
+    |> Enum.into(%{})
     |> ensure_reachable
+    |> Enum.into(%{}, fn {k, v} -> {k, to_string(v)} end)
+    |> write_out
   end
+
+  def write_out(swaps) do
+    IO.inspect(swaps)
+
+    new_item_constants = File.read!("./pokecrystal/constants/item_constants.asm")
+    |> String.replace("BASEMENT_KEY", swaps[:BASEMENT_KEY], global: false)
+    |> String.replace("BICYCLE", swaps[:BICYCLE], global: false)
+    |> String.replace("BLUE_CARD", swaps[:BLUE_CARD], global: false)
+    |> String.replace("CARD_KEY", swaps[:CARD_KEY], global: false)
+    |> String.replace("CLEAR_BELL", swaps[:CLEAR_BELL], global: false)
+    |> String.replace("COIN_CASE", swaps[:COIN_CASE], global: false)
+    |> String.replace("GOOD_ROD", swaps[:GOOD_ROD], global: false)
+    |> String.replace("HM_CUT", swaps[:HM_CUT], global: false)
+    |> String.replace("HM_FLASH", swaps[:HM_FLASH], global: false)
+    |> String.replace("HM_FLY", swaps[:HM_FLY], global: false)
+    |> String.replace("HM_STRENGTH", swaps[:HM_STRENGTH], global: false)
+    |> String.replace("HM_SURF", swaps[:HM_SURF], global: false)
+    |> String.replace("HM_WATERFALL", swaps[:HM_WATERFALL], global: false)
+    |> String.replace("HM_WHIRLPOOL", swaps[:HM_WHIRLPOOL], global: false)
+    |> String.replace("ITEMFINDER", swaps[:ITEMFINDER], global: false)
+    |> String.replace("LOST_ITEM", swaps[:LOST_ITEM], global: false)
+    |> String.replace("MACHINE_PART", swaps[:MACHINE_PART], global: false)
+    |> String.replace("MYSTERY_EGG", swaps[:MYSTERY_EGG], global: false)
+    |> String.replace("OLD_ROD", swaps[:OLD_ROD], global: false)
+    |> String.replace("PASS", swaps[:PASS], global: false)
+    |> String.replace("RED_SCALE", swaps[:RED_SCALE], global: false)
+    |> String.replace("SECRETPOTION", swaps[:SECRETPOTION], global: false)
+    |> String.replace("SILVER_WING", swaps[:SILVER_WING], global: false)
+    |> String.replace("SQUIRTBOTTLE", swaps[:SQUIRTBOTTLE], global: false)
+    |> String.replace("SUPER_ROD", swaps[:SUPER_ROD], global: false)
+    |> String.replace("S_S_TICKET", swaps[:S_S_TICKET], global: false)
+
+
+    # something like this
+    # item_attrs_asm =': item_attrs_asm = File.read!("./pokecrystal/items/item_attributes.asm")
+    # Regex.replace(~r/(MASTER\sBALL\n)(\t.*)/, item_attrs_asm, "\\1#{CrystalKeyItemRandomizer.item_attributes[:HM_CUT]}")
+    # barf
+    new_item_attributes = File.read!("./pokecrystal/items/item_attributes.asm")
+    |> String.replace(~r/(BASEMENT_KEY\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:BASEMENT_KEY])]}", global: false)
+    |> String.replace(~r/(BICYCLE\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:BICYCLE])]}", global: false)
+    |> String.replace(~r/(BLUE_CARD\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:BLUE_CARD])]}", global: false)
+    |> String.replace(~r/(CARD_KEY\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:CARD_KEY])]}", global: false)
+    |> String.replace(~r/(CLEAR_BELL\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:CLEAR_BELL])]}", global: false)
+    |> String.replace(~r/(COIN_CASE\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:COIN_CASE])]}", global: false)
+    |> String.replace(~r/(GOOD_ROD\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:GOOD_ROD])]}", global: false)
+    |> String.replace(~r/(HM_CUT\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:HM_CUT])]}", global: false)
+    |> String.replace(~r/(HM_FLASH\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:HM_FLASH])]}", global: false)
+    |> String.replace(~r/(HM_FLY\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:HM_FLY])]}", global: false)
+    |> String.replace(~r/(HM_STRENGTH\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:HM_STRENGTH])]}", global: false)
+    |> String.replace(~r/(HM_SURF\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:HM_SURF])]}", global: false)
+    |> String.replace(~r/(HM_WATERFALL\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:HM_WATERFALL])]}", global: false)
+    |> String.replace(~r/(HM_WHIRLPOOL\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:HM_WHIRLPOOL])]}", global: false)
+    |> String.replace(~r/(ITEMFINDER\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:ITEMFINDER])]}", global: false)
+    |> String.replace(~r/(LOST_ITEM\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:LOST_ITEM])]}", global: false)
+    |> String.replace(~r/(MACHINE_PART\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:MACHINE_PART])]}", global: false)
+    |> String.replace(~r/(MYSTERY_EGG\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:MYSTERY_EGG])]}", global: false)
+    |> String.replace(~r/(OLD_ROD\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:OLD_ROD])]}", global: false)
+    |> String.replace(~r/(PASS\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:PASS])]}", global: false)
+    |> String.replace(~r/(RED_SCALE\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:RED_SCALE])]}", global: false)
+    |> String.replace(~r/(SECRETPOTION\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:SECRETPOTION])]}", global: false)
+    |> String.replace(~r/(SILVER_WING\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:SILVER_WING])]}", global: false)
+    |> String.replace(~r/(SQUIRTBOTTLE\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:SQUIRTBOTTLE])]}", global: false)
+    |> String.replace(~r/(SUPER_ROD\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:SUPER_ROD])]}", global: false)
+    |> String.replace(~r/(S_S_TICKET\n)(\t.*)/, "\\1#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:S_S_TICKET])]}", global: false)
+
+    File.write!("./new_item_constants", new_item_constants)
+    File.write!("./new_item_attributes", new_item_attributes)
+  end
+
 
   def ensure_reachable(swaps) do
     reachability = CrystalKeyItemRandomizer.Reachability.analyze(swaps)
@@ -167,8 +237,8 @@ SQUIRTBOTTLE: => "	item_attribute     0, 0,                 0, CANT_SELECT | CAN
 
   defp copy_item_constants_file do
     File.copy!(
-      './pokecrystal/constants/item_constants.asm',
-      './pokecrystal/constants/item_constants.asm.tmp'
+      "./pokecrystal/constants/item_constants.asm",
+      "./pokecrystal/constants/item_constants.asm.tmp"
     )
   end
 end
