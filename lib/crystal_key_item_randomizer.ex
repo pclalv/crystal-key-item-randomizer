2f766feffdc929d1745aedef3531fdaefe0741a4
paul alvarez <palvarez@greenhouse.io>
2017-10-18 00:05:59 -0400

    write_out_item_names works

--- lib/crystal_key_item_randomizer.ex
+++ lib/crystal_key_item_randomizer.ex
@@ -113,6 +113,35 @@ defmodule CrystalKeyItemRandomizer do
     SQUIRTBOTTLE: "	item_attribute     0, 0,                 0, CANT_SELECT | CANT_TOSS, KEY_ITEM, ITEMMENU_CLOSE,   ITEMMENU_NOUSE",
   }
 
+  @ugly_pretty_names %{
+    BASEMENT_KEY: "BASEMENT KEY",
+    BICYCLE: "BICYCLE",
+    BLUE_CARD: "BLUE CARD",
+    CARD_KEY: "CARD KEY",
+    CLEAR_BELL: "CLEAR BELL",
+    COIN_CASE: "COIN CASE",
+    GOOD_ROD: "GOOD ROD",
+    HM_CUT: "HM01",
+    HM_FLASH: "HM05",
+    HM_FLY: "HM02",
+    HM_STRENGTH: "HM04",
+    HM_SURF: "HM03",
+    HM_WATERFALL: "HM07",
+    HM_WHIRLPOOL: "HM06",
+    ITEMFINDER: "ITEMFINDER",
+    LOST_ITEM: "LOST ITEM",
+    MACHINE_PART: "MACHINE PART",
+    MYSTERY_EGG: "MYSTERY EGG",
+    OLD_ROD: "OLD ROD",
+    PASS: "PASS",
+    RED_SCALE: "RED SCALE",
+    SECRETPOTION: "SECRETPOTION",
+    SILVER_WING: "SILVER WING",
+    SQUIRTBOTTLE: "SQUIRTBOTTLE",
+    SUPER_ROD: "SUPER ROD",
+    S_S_TICKET: "S.S.TICKET",
+  }
+
   def required_items, do: @required_items
   def maybe_required_items, do: @maybe_required_items
   def non_required_items, do: @non_required_items
@@ -122,8 +151,17 @@ defmodule CrystalKeyItemRandomizer do
   def surf_blocked_items, do: @surf_blocked_items
   def sudowoodo_blocked_items, do: key_items() -- pre_sudowoodo_items()
   def item_attributes, do: @item_attributes
+  def ugly_pretty_names, do: @ugly_pretty_names
 
   def key_items, do: required_items() ++ maybe_required_items() ++ non_required_items()
+
+  @item_attributes_path "./pokecrystal/items/item_attributes.asm"
+  @item_constants_path "./pokecrystal/constants/item_constants.asm"
+  @item_names_path "./pokecrystal/items/item_names.asm"
+
+  def item_attributes_path, do: @item_attributes_path
+  def item_constants_path, do: @item_constants_path
+  def item_names_path, do: @item_names_path
 
   @doc """
   Run.
@@ -146,12 +184,12 @@ defmodule CrystalKeyItemRandomizer do
   def write_out_all(swaps) do
     write_out_item_constants(swaps)
     write_out_item_attributes(swaps)
-    #write_out_item_names(swaps)
+    write_out_item_names(swaps)
     #write_out_item_effects(swaps)
   end
 
   def write_out_item_constants(swaps) do
-    new_item_constants = File.read!("./pokecrystal/constants/item_constants.asm")
+    new_item_constants = File.read!(item_constants_path)
     # hack because i don't know how negative regex matching would work
     |> String.replace("BASEMENT_KEY", "BASEMENT_KEY;original", global: false)
     |> String.replace("BICYCLE", "BICYCLE;original", global: false)
@@ -207,7 +245,7 @@ defmodule CrystalKeyItemRandomizer do
     |> String.replace(~r/\w+\sSUPER_ROD;original/, item_constant(swaps[:SUPER_ROD]), global: false)
     |> String.replace(~r/\w+\sS_S_TICKET;original/, item_constant(swaps[:S_S_TICKET]), global: false)
 
-    File.write!("./pokecrystal/constants/item_constants.asm", new_item_constants)
+    File.write!(item_constants_path, new_item_constants)
   end
 
   def write_out_item_attributes(swaps) do
@@ -215,8 +253,7 @@ defmodule CrystalKeyItemRandomizer do
     # item_attrs_asm =': item_attrs_asm = File.read!("./pokecrystal/items/item_attributes.asm")
     # Regex.replace(~r/(MASTER\sBALL\n)(\t.*)/, item_attrs_asm, "\\1#{CrystalKeyItemRandomizer.item_attributes[:HM_CUT]}")
     # barf
-    new_item_attributes = File.read!("./pokecrystal/items/item_attributes.asm")
-    |> String.replace(~r/(BASEMENT KEY)(\n\t.*)/, "\\1;original\\2", global: false)
+    new_item_attributes = File.read!(item_attributes_path)
     |> String.replace(~r/(BASEMENT KEY)(\n\t.*)/, "\\1;original\\2", global: false)
     |> String.replace(~r/(BICYCLE)(\n\t.*)/, "\\1;original\\2", global: false)
     |> String.replace(~r/(BLUE CARD)(\n\t.*)/, "\\1;original\\2", global: false)
@@ -245,7 +282,6 @@ defmodule CrystalKeyItemRandomizer do
     |> String.replace(~r/(S.S.TICKET)(\n\t.*)/, "\\1;original\\2", global: false)
     # similar hack
     |> String.replace(~r/(BASEMENT KEY);original\n(\t.*)/, "\\1\n#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:BASEMENT_KEY])]}", global: false)
-    |> String.replace(~r/(BASEMENT KEY);original\n(\t.*)/, "\\1\n#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:BASEMENT_KEY])]}", global: false)
     |> String.replace(~r/(BICYCLE);original\n(\t.*)/, "\\1\n#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:BICYCLE])]}", global: false)
     |> String.replace(~r/(BLUE CARD);original\n(\t.*)/, "\\1\n#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:BLUE_CARD])]}", global: false)
     |> String.replace(~r/(CARD KEY);original\n(\t.*)/, "\\1\n#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:CARD_KEY])]}", global: false)
@@ -272,11 +308,67 @@ defmodule CrystalKeyItemRandomizer do
     |> String.replace(~r/(SUPER ROD);original\n(\t.*)/, "\\1\n#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:SUPER_ROD])]}", global: false)
     |> String.replace(~r/(S.S.TICKET);original\n(\t.*)/, "\\1\n#{CrystalKeyItemRandomizer.item_attributes[String.to_existing_atom(swaps[:S_S_TICKET])]}", global: false)
 
-    File.write!("./pokecrystal/items/item_attributes.asm", new_item_attributes)
+    File.write!(item_attributes_path, new_item_attributes)
   end
 
   def write_out_item_names(swaps) do
-    
+    new_item_names = File.read!(item_names_path)
+    |> String.replace(~r/(BASEMENT KEY@")/, "\\1;original", global: false)
+    |> String.replace(~r/(BICYCLE@")/, "\\1;original", global: false)
+    |> String.replace(~r/(BLUE CARD@")/, "\\1;original", global: false)
+    |> String.replace(~r/(CARD KEY@")/, "\\1;original", global: false)
+    |> String.replace(~r/(CLEAR BELL@")/, "\\1;original", global: false)
+    |> String.replace(~r/(COIN CASE@")/, "\\1;original", global: false)
+    |> String.replace(~r/(GOOD ROD@")/, "\\1;original", global: false)
+    |> String.replace(~r/(HM01@")/, "\\1;original", global: false)
+    |> String.replace(~r/(HM05@")/, "\\1;original", global: false)
+    |> String.replace(~r/(HM02@")/, "\\1;original", global: false)
+    |> String.replace(~r/(HM04@")/, "\\1;original", global: false)
+    |> String.replace(~r/(HM03@")/, "\\1;original", global: false)
+    |> String.replace(~r/(HM07@")/, "\\1;original", global: false)
+    |> String.replace(~r/(HM06@")/, "\\1;original", global: false)
+    |> String.replace(~r/(ITEMFINDER@")/, "\\1;original", global: false)
+    |> String.replace(~r/(LOST ITEM@")/, "\\1;original", global: false)
+    |> String.replace(~r/(MACHINE PART@")/, "\\1;original", global: false)
+    |> String.replace(~r/(MYSTERY EGG@")/, "\\1;original", global: false)
+    |> String.replace(~r/(OLD ROD@")/, "\\1;original", global: false)
+    |> String.replace(~r/(PASS@")/, "\\1;original", global: false)
+    |> String.replace(~r/(RED SCALE@")/, "\\1;original", global: false)
+    |> String.replace(~r/(SECRETPOTION@")/, "\\1;original", global: false)
+    |> String.replace(~r/(SILVER WING@")/, "\\1;original", global: false)
+    |> String.replace(~r/(SQUIRTBOTTLE@")/, "\\1;original", global: false)
+    |> String.replace(~r/(SUPER ROD@")/, "\\1;original", global: false)
+    |> String.replace(~r/(S.S.TICKET@")/, "\\1;original", global: false)
+    # similar hack
+    |> String.replace("BASEMENT KEY@\";original", "#{to_pretty_name(swaps[:BASEMENT_KEY])}@\"", global: false)
+    |> String.replace("BASEMENT KEY@\";original", "#{to_pretty_name(swaps[:BASEMENT_KEY])}@\"", global: false)
+    |> String.replace("BICYCLE@\";original", "#{to_pretty_name(swaps[:BICYCLE])}@\"", global: false)
+    |> String.replace("BLUE CARD@\";original", "#{to_pretty_name(swaps[:BLUE_CARD])}@\"", global: false)
+    |> String.replace("CARD KEY@\";original", "#{to_pretty_name(swaps[:CARD_KEY])}@\"", global: false)
+    |> String.replace("CLEAR BELL@\";original", "#{to_pretty_name(swaps[:CLEAR_BELL])}@\"", global: false)
+    |> String.replace("COIN CASE@\";original", "#{to_pretty_name(swaps[:COIN_CASE])}@\"", global: false)
+    |> String.replace("GOOD ROD@\";original", "#{to_pretty_name(swaps[:GOOD_ROD])}@\"", global: false)
+    |> String.replace("HM01@\";original", "#{to_pretty_name(swaps[:HM_CUT])}@\"", global: false)
+    |> String.replace("HM05@\";original", "#{to_pretty_name(swaps[:HM_FLASH])}@\"", global: false)
+    |> String.replace("HM02@\";original", "#{to_pretty_name(swaps[:HM_FLY])}@\"", global: false)
+    |> String.replace("HM04@\";original", "#{to_pretty_name(swaps[:HM_STRENGTH])}@\"", global: false)
+    |> String.replace("HM03@\";original", "#{to_pretty_name(swaps[:HM_SURF])}@\"", global: false)
+    |> String.replace("HM07@\";original", "#{to_pretty_name(swaps[:HM_WATERFALL])}@\"", global: false)
+    |> String.replace("HM06@\";original", "#{to_pretty_name(swaps[:HM_WHIRLPOOL])}@\"", global: false)
+    |> String.replace("ITEMFINDER@\";original", "#{to_pretty_name(swaps[:ITEMFINDER])}@\"", global: false)
+    |> String.replace("LOST ITEM@\";original", "#{to_pretty_name(swaps[:LOST_ITEM])}@\"", global: false)
+    |> String.replace("MACHINE PART@\";original", "#{to_pretty_name(swaps[:MACHINE_PART])}@\"", global: false)
+    |> String.replace("MYSTERY EGG@\";original", "#{to_pretty_name(swaps[:MYSTERY_EGG])}@\"", global: false)
+    |> String.replace("OLD ROD@\";original", "#{to_pretty_name(swaps[:OLD_ROD])}@\"", global: false)
+    |> String.replace("PASS@\";original", "#{to_pretty_name(swaps[:PASS])}@\"", global: false)
+    |> String.replace("RED SCALE@\";original", "#{to_pretty_name(swaps[:RED_SCALE])}@\"", global: false)
+    |> String.replace("SECRETPOTION@\";original", "#{to_pretty_name(swaps[:SECRETPOTION])}@\"", global: false)
+    |> String.replace("SILVER WING@\";original", "#{to_pretty_name(swaps[:SILVER_WING])}@\"", global: false)
+    |> String.replace("SQUIRTBOTTLE@\";original", "#{to_pretty_name(swaps[:SQUIRTBOTTLE])}@\"", global: false)
+    |> String.replace("SUPER ROD@\";original", "#{to_pretty_name(swaps[:SUPER_ROD])}@\"", global: false)
+    |> String.replace("S.S.TICKET@\";original", "#{to_pretty_name(swaps[:S_S_TICKET])}@\"", global: false)
+
+    File.write!(item_names_path, new_item_names)
   end
 
   def write_out_item_effects(swaps) do
@@ -311,5 +403,10 @@ defmodule CrystalKeyItemRandomizer do
     else
       "const #{item}"
     end
+  end
+
+  defp to_pretty_name(item) when is_bitstring(item), do: to_pretty_name(String.to_existing_atom(item))
+  defp to_pretty_name(item) when is_atom(item) do
+    CrystalKeyItemRandomizer.ugly_pretty_names[item]
   end
 end
