3f8241e72cb16e77a2968c0263bca822890f9c44
paul alvarez <palvarez@greenhouse.io>
2017-12-27 08:04:22 -0500

    actually start testing this thing!

--- test/crystal_key_item_randomizer_test.exs
+++ test/crystal_key_item_randomizer_test.exs
@@ -1,8 +1,4 @@
 defmodule CrystalKeyItemRandomizerTest do
   use ExUnit.Case
   doctest CrystalKeyItemRandomizer
-
-  test "greets the world" do
-    assert CrystalKeyItemRandomizer.hello() == :world
-  end
 end
