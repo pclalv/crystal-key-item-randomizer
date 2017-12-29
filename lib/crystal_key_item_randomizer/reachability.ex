c0fb2842664aecb32aefc6f787cb24f90cd3ad57
paul alvarez <palvarez@greenhouse.io>
2017-12-29 01:08:42 -0500

    try to bring old and new reachabilit analyses up to par

--- lib/crystal_key_item_randomizer/reachability.ex
+++ lib/crystal_key_item_randomizer/reachability.ex
@@ -13,9 +13,31 @@ defmodule CrystalKeyItemRandomizer.Reachability do
     }
   end
 
+  def analyze_old(swaps) do
+    %CrystalKeyItemRandomizer.Reachability{
+      ss_locked?: ss_locked_old?(swaps),
+      kanto_locked?: kanto_locked_old?(swaps),
+      goldenrod_locked?: goldenrod_locked_old?(swaps),
+      surf_locked?: surf_locked_old?(swaps),
+      tree_locked?: tree_locked_old?(swaps),
+    }
+  end
+
+  def compare_old_and_new(swaps) do
+    analyze(swaps) == analyze_old(swaps)
+  end
+
   # the seed is ss-locked if the `S_S_TICKET` is replaced by any
   # required key item.
   defp ss_locked?(swaps) do
+    state_machine = Diet.Stepper.new(CrystalKeyItemRandomizer.Reachability.SSLocked, nil)
+    case Diet.Stepper.run(state_machine, {:begin, swaps}) do
+      {{:ss_locked, _}, _} -> true
+      {{:ok, _}, _} -> false
+    end
+  end
+
+  defp ss_locked_old?(swaps) do
     replacement = swaps[:S_S_TICKET]
 
     cond do
@@ -27,6 +49,10 @@ defmodule CrystalKeyItemRandomizer.Reachability do
         true
       replacement == :LOST_ITEM && required?(swaps[:PASS]) ->
         true
+      replacement == :MACHINE_PART && required?(swaps[:PASS]) ->
+        true
+      replacement == :MACHINE_PART && required?(swaps[:SILVER_WING]) ->
+        true
       true ->
         false
     end
@@ -35,16 +61,28 @@ defmodule CrystalKeyItemRandomizer.Reachability do
   # the seed is kanto-locked if kanto is inaccessible but any required
   # key item is in kanto.
   defp kanto_locked?(swaps) do
+    state_machine = Diet.Stepper.new(CrystalKeyItemRandomizer.Reachability.KantoLocked, nil)
+    case Diet.Stepper.run(state_machine, {:begin, swaps}) do
+      {{:kanto_locked, _}, _} -> true
+      {{:ok, _}, _} -> false
+    end
+  end
+
+  defp kanto_locked_old?(swaps) do
     if in_kanto?(swaps, :PASS) && in_kanto?(swaps, :S_S_TICKET) do
       cond do
         any_required_item_in_kanto?(swaps) ->
           true
-        # the following three cases check maybe-required items.
+        # the following cases check maybe-required items.
         in_kanto?(swaps, :BASEMENT_KEY) && required?(swaps[:CARD_KEY]) ->
           true
         in_kanto?(swaps, :CARD_KEY) && required?(swaps[:CLEAR_BELL]) ->
           true
         in_kanto?(swaps, :LOST_ITEM) && required?(swaps[:PASS]) ->
+          true
+        in_kanto?(swaps, :MACHINE_PART) && required?(swaps[:PASS]) ->
+          true
+        in_kanto?(swaps, :MACHINE_PART) && required?(swaps[:SILVER_WING]) ->
           true
         true ->
           false
@@ -55,14 +93,22 @@ defmodule CrystalKeyItemRandomizer.Reachability do
   end
 
   defp any_required_item_in_kanto?(swaps) do
-    CrystalKeyItemRandomizer.required_items
+    CrystalKeyItemRandomizer.required_item_names
     |> Enum.any?(fn(required_item) -> in_kanto?(swaps, required_item) end)
+  end
+
+  defp goldenrod_locked?(swaps) do
+    state_machine = Diet.Stepper.new(CrystalKeyItemRandomizer.Reachability.GoldenrodLocked, nil)
+    case Diet.Stepper.run(state_machine, {:begin, swaps}) do
+      {{:goldenrod_locked, _}, _} -> true
+      {{:ok, _}, _} -> false
+    end
   end
 
   # the seed is goldenrod-locked if the player reaches goldenrod and
   # can make progress neither by clearing sudowoodo nor by traveling to
   # kanto.
-  defp goldenrod_locked?(swaps) do
+  defp goldenrod_locked_old?(swaps) do
     cond do
       CrystalKeyItemRandomizer.pre_goldenrod_items |> Enum.any?( &(swaps[&1] == :SQUIRTBOTTLE) ) ->
         false
@@ -106,14 +152,30 @@ defmodule CrystalKeyItemRandomizer.Reachability do
     end
   end
 
-  # the seed is surf-locked if any surf-blocked item is replaced by `HM_SURF`.
   defp surf_locked?(swaps) do
+    state_machine = Diet.Stepper.new(CrystalKeyItemRandomizer.Reachability.SurfLocked, nil)
+    case Diet.Stepper.run(state_machine, {:begin, swaps}) do
+      {{:surf_locked, _}, _} -> true
+      {{:ok, _}, _} -> false
+    end
+  end
+
+  # the seed is surf-locked if any surf-blocked item is replaced by `HM_SURF`.
+  defp surf_locked_old?(swaps) do
     CrystalKeyItemRandomizer.surf_blocked_items\
     |> Enum.any?( &(swaps[&1] == :HM_SURF) )
   end
 
-  # the seed is tree-locked if none of the pre-tree items are HM_CUT or the SQUIRTBOTTLE.
   defp tree_locked?(swaps) do
+    state_machine = Diet.Stepper.new(CrystalKeyItemRandomizer.Reachability.TreeLocked, nil)
+    case Diet.Stepper.run(state_machine, {:begin, swaps}) do
+      {{:tree_locked, _}, _} -> true
+      {{:ok, _}, _} -> false
+    end
+  end
+
+  # the seed is tree-locked if none of the pre-tree items are HM_CUT or the SQUIRTBOTTLE.
+  defp tree_locked_old?(swaps) do
     !Enum.any?(
       CrystalKeyItemRandomizer.pre_tree_items,
       &( swaps[&1] == :HM_CUT || swaps[&1] == :SQUIRTBOTTLE )
