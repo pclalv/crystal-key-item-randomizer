8802028704ef86b73adb5220483b3776088a3977
paul alvarez <palvarez@greenhouse.io>
2017-12-29 11:09:50 -0500

    add tree locked example

--- test/crystal_key_item_randomizer/reachability/tree_locked_test.exs
+++ test/crystal_key_item_randomizer/reachability/tree_locked_test.exs
@@ -3,11 +3,19 @@ defmodule CrystalKeyItemRandomizer.Reachability.TreeLockedTest do
   alias CrystalKeyItemRandomizer.Reachability.TreeLocked
   doctest TreeLocked
 
-  test "works with the vanilla swaps" do
+  test "the vanilla swaps are ok" do
     swaps = CrystalKeyItemRandomizer.vanilla_swaps
     result = Diet.Stepper.new(TreeLocked, nil)
     |> Diet.Stepper.run({:begin, swaps})
 
     assert {{:ok, ^swaps}, _} = result
+  end
+
+  test "a swap where neither HM_CUT nor SQUIRTBOTTLE is available early" do
+    swaps = for item <- CrystalKeyItemRandomizer.pre_tree_items, do: {item, :FOO}, into: %{}
+    result = Diet.Stepper.new(TreeLocked, nil)
+    |> Diet.Stepper.run({:begin, swaps})
+
+    assert {{:tree_locked, ^swaps}, _} = result
   end
 end
