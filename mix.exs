232ff63ba07ad8d64e6540345dcd52ac2ec72d2c
paul alvarez <palvarez@greenhouse.io>
2018-05-24 20:25:51 -0400

    update mixfile, place in umbrella

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
-defmodule CrystalKeyItemRandomizer.Mixfile do
+defmodule LockDetector.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :crystal_key_item_randomizer,
+      app: :lock_detector,
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
     ]
   end
 
