b2f66b534e0e80b2b826e4a90708535d99edeefd
paul alvarez <palvarez@greenhouse.io>
2019-08-31 19:51:59 -0400

    Upgrade elixir version

--- apps/swap_generator/mix.exs
+++ apps/swap_generator/mix.exs
@@ -5,7 +5,7 @@ defmodule SwapGenerator.Mixfile do
     [
       app: :swap_generator,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       build_path: "../../_build",
