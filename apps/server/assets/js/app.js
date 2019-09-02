daad9f78efadfc02b4e387e1a45559c7faa8796d
paul alvarez <palvarez@greenhouse.io>
2019-09-02 12:23:11 -0400

    Permit use of all HMs without badges

--- apps/server/assets/js/app.js
+++ apps/server/assets/js/app.js
@@ -140,6 +140,7 @@ window.Events = [
         "originalValue": 205,
         "replacementValue": EVENT_ROUTE_30_BATTLE
     },
+
     // travel via SS Aqua on any day
     {
         "name": "IFEQUAL_WEEKDAY_SATURDAY", 
@@ -170,6 +171,125 @@ window.Events = [
         "address": 477516,
         "originalValue": 129, // .NextShipFriday
         "replacementValue": FIRST_TIME
+    },
+
+    // always be able to use fly
+    {
+	"name": "FlyFunction.DisableCheckBadge0",
+	"address": 51797,
+        "replacementValue": NOP
+    },
+    {
+	"name": "FlyFunction.DisableCheckBadge1",
+	"address": 51798,
+        "replacementValue": NOP
+    },
+    {
+	"name": "FlyFunction.DisableCheckBadge2",
+	"address": 51799,
+        "replacementValue": NOP
+    },
+
+    // always be able to use flash
+    {
+	"name": "OWFlash.DisableCheckBadge0",
+	"address": 51384,
+	"replacementValue": NOP
+    },
+    {
+	"name": "OWFlash.DisableCheckBadge1",
+	"address": 51385,
+	"replacementValue": NOP
+    },
+    {
+	"name": "OWFlash.DisableCheckBadge2",
+	"address": 51386,
+	"replacementValue": NOP
+    },
+
+    // always be able to use strength
+    {
+	"name": "StrengthFunction.DisableCheckBadge0",
+	"address": 52465,
+	"replacementValue": NOP
+    },
+    {
+	"name": "StrengthFunction.DisableCheckBadge1",
+	"address": 52466,
+	"replacementValue": NOP
+    },
+    {
+	"name": "StrengthFunction.DisableCheckBadge2",
+	"address": 52467
+	"replacementValue": NOP
+    },
+
+    // always be able to use surf
+    {
+        "name": "SurfFunction.Jumptable.DisableCheckBadge0",
+        "address": 51493,
+        "replacementValue": NOP
+    },
+    {
+        "name": "SurfFunction.Jumptable.DisableCheckBadge1",
+        "address": 51494,
+        "replacementValue": NOP
+    },
+    {
+        "name": "SurfFunction.Jumptable.DisableCheckBadge2",
+        "address": 51495,
+        "replacementValue": NOP
+    },
+
+    // always be able to use whirlpool
+    {
+	"name": "WhirlpoolFunction.Jumptable_cdae.DisableCheckBadge0",
+	"address": 52663,
+        "replacementValue": NOP
+    },
+    {
+	"name": "WhirlpoolFunction.Jumptable_cdae.DisableCheckBadge1",
+	"address": 52664,
+        "replacementValue": NOP
+    },
+    {
+	"name": "WhirlpoolFunction.Jumptable_cdae.DisableCheckBadge2",
+	"address": 52665,
+        "replacementValue": NOP
+    }
+
+     // always be able to use waterfall
+    {
+	"name": "TryWaterfallOW.DisableCheckBadge0",
+	"address": 52064,
+        "replacementValue": NOP
+    },
+    {
+	"name": "TryWaterfallOW.DisableCheckBadge1",
+	"address": 52065,
+        "replacementValue": NOP
+    },
+    {
+	"name": "TryWaterfallOW.DisableCheckBadge2",
+	"address": 52066,
+        "replacementValue": NOP
+    },
+
+    // always be able to use cut
+    {
+	"name": "TryCutOW.DisableCheckBadge0",
+	"address:" 53648,
+        "replacementValue": NOP
+    },
+    {
+	"name": "TryCutOW.DisableCheckBadge1",
+	"address:" 53649,
+        "replacementValue": NOP
+    },
+    {
+	"name": "TryCutOW.DisableCheckBadge2",
+	"address:" 53650,
+        "replacementValue": NOP
     }
 ];
 
