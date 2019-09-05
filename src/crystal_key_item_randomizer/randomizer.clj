ad6d749069e01827f83b268fc8990708d10e0e27
paul alvarez <palvarez@greenhouse.io>
2019-09-05 08:23:05 -0400

    take a bunch of notes on these items

--- src/crystal_key_item_randomizer/randomizer.clj
+++ src/crystal_key_item_randomizer/randomizer.clj
@@ -2,16 +2,28 @@
 
 (def required-items
   {:HM_SURF {:name :HM_SURF
-             :location :DanceTheatre}
+             :location :DanceTheatre
+             :comment "required for Cianwood badge"}
    :HM_STRENGTH {:name :HM_STRENGTH
-                 :location :OlivineCafe}
-   :HM_WHIRLPOOL {:name :HM_WHIRLPOOL
-                  :location :TeamRocketBaseB2F}
+                 :location :OlivineCafe
+                 :comment "required for Victory Road"}
+   :SECRETPOTION {:name :SECRETPOTION
+                  :location :CianwoodPharmacy
+                  :comment "required for Olivine badge"}
+
+   ;; HM_WATERFALL is required to cross Tohjo Falls - or wait, can the
+   ;; player perhaps get through from Viridian?
+
+   ;; i believe it should be possible to reach the League for the
+   ;; first time from Viridian. a player having HM_SURF would be able
+   ;; to complete the Power Plant sidequest, obtain the Radio Expansion Card,
+   ;; wake the Snorlax and get to Viridian.
+
+   ;; all that's really required is the Radio Expansion Card, but
+   ;; we're not to the point of randomizing that yet.
    :HM_WATERFALL {:name :HM_WATERFALL
                   :location :IcePath1F
-                  :macro :itemball}
-   :SECRETPOTION {:name :SECRETPOTION
-                  :location :CianwoodPharmacy}})
+                  :macro :itemball}})
 
 (def maybe-required-items
   {:BASEMENT_KEY {:name :BASEMENT_KEY
@@ -22,68 +34,63 @@
             :location :IlexForest}
    :LOST_ITEM {:name :LOST_ITEM
                :location :PokemonFanClub}
-   :PASS {:name :PASS
-          :location :CopycatsHouse2F}
-   :S_S_TICKET {:name :S_S_TICKET
-                :location :ElmsLab}
-   :SQUIRTBOTTLE {:name :SQUIRTBOTTLE
-                  :location :GoldenrodFlowerShop}
-   :RED_SCALE {:name :RED_SCALE
-               :location :LakeofRage}
-
    :MACHINE_PART {:name :MACHINE_PART
                   :location :CeruleanGym
-                  :macro "dwb EVENT_FOUND_MACHINE_PART_IN_CERULEAN_GYM"}})
+                  :macro "dwb EVENT_FOUND_MACHINE_PART_IN_CERULEAN_GYM"}
+
+   ;; probably not required?? TODO: settle this!
+   :HM_WHIRLPOOL {:name :HM_WHIRLPOOL
+                  :location :TeamRocketBaseB2F}
+
+   ;; the player might need to go to kanto.
+   :PASS {:name :PASS
+          :location :CopycatsHouse2F}
+
+   ;; the player might need to goto kanto.
+   :S_S_TICKET {:name :S_S_TICKET
+                :location :ElmsLab}
+
+   ;; the player might be able to work around Sudowoodo with PASS and
+   ;; S_S_TICKET
+   :SQUIRTBOTTLE {:name :SQUIRTBOTTLE
+                  :location :GoldenrodFlowerShop}
+
+   ;; i forget why RED_SCALE is maybe required...
+   :RED_SCALE {:name :RED_SCALE
+               :location :LakeofRage}})
 
 (def maybe-required-pairs
-  [;; if the thing on the left is in kanto and the thing on
-   ;; the right is required, then the swap is kanto-locked
-   {:BASEMENT_KEY :CARD_KEY}
+  [{:BASEMENT_KEY :CARD_KEY}
    {:CARD_KEY :CLEAR_BELL}
-   {:MACHINE_PART :SILVER_WING}
-   ;; this is a bit of a special case. CARD_KEY and CLEAR_BERLL have
-   ;; other prereqs, but those prereqs are themselves required
-   ;; (e.g. HM_CUT or SQUIRTBOTTLE.)
-   {[:LOST_ITEM :MACHINE_PART] :PASS}])
+   {[:LOST_ITEM :MACHINE_PART] :PASS}
+   ;; TODO: wtf is this about, we don't care about SILVER_WING
+   {:MACHINE_PART :SILVER_WING}])
 
 (def non-required-items
-  {;; HMs
+  {:BICYCLE {:name :BICYCLE
+             :location :GoldenrodBikeShop}
+   :BLUE_CARD {:name :BLUE_CARD
+               :location :RadioTower2F}
+   :CLEAR_BELL {:name :CLEAR_BELL
+                :location :RadioTower5F}
+   :COIN_CASE {:name :COIN_CASE
+               :location :GoldenrodUnderground
+               :macro :itemball}
+   :GOOD_ROD {:name :GOOD_ROD
+              :location :OlivineGoodRodHouse}
    :HM_FLASH {:name :HM_FLASH
               :location :SproutTower3F}
    :HM_FLY {:name :HM_FLY
             :location :CianwoodCity}
-
-   ;; non-HMs
-   :BICYCLE {:name :BICYCLE
-             :location :GoldenrodBikeShop}
-
-   ;; useless
-   :BLUE_CARD {:name :BLUE_CARD
-               :location :RadioTower2F}
-
-   :CLEAR_BELL {:name :CLEAR_BELL
-                :location :RadioTower5F}
-
-   :COIN_CASE {:name :COIN_CASE
-               :location :GoldenrodUnderground
-               :macro :itemball}
-
-   :GOOD_ROD {:name :GOOD_ROD
-              :location :OlivineGoodRodHouse}
-
    :ITEMFINDER {:name :ITEMFINDER
                 :location :EcruteakItemfinderHouse}
-
-   ;; useless; blocking battle will be disabled
    :MYSTERY_EGG {:name :MYSTERY_EGG
-                 :location :MrPokemonsHouse}
-
+                 :location :MrPokemonsHouse
+                 :comment "no longer required; the 'Important Battle' is disabled"}
    :OLD_ROD {:name :OLD_ROD
              :location :Route32PokeCenter1F}
-
    :SILVER_WING {:name :SILVER_WING
                  :location :PewterCity}
-
    :SUPER_ROD {:name :SUPER_ROD
                :location :Route12SuperRodHouse}})
 
@@ -99,33 +106,35 @@
                      :OLD_ROD])
 
 (def pre-goldenrod-items
-  ;; `CARD_KEY` is not included in pre_goldenrod_items given the
-  ;; current state of the underlying assembly! this is because even if
-  ;; you have the `BASEMENT_KEY`, the basement will be devoid of any
-  ;; rockets
+  ;; `CARD_KEY` is not included in pre_goldenrod_items! this is
+  ;; because even if you have the `BASEMENT_KEY`, the basement will be
+  ;; devoid of Team Rockets.
 
-  ;; `CLEAR_BELL` is not included because i believe that the same goes
-  ;; for the `CARD_KEY`/radio tower upper floors; even if it's not
-  ;; true, i bet it'd make things more complicated, so i'm just
+  ;; `CLEAR_BELL` is likewise not included because i believe that the
+  ;; same goes for the `CARD_KEY`/radio tower upper floors. i'm just
   ;; considering the `CLEAR_BELL` inaccessible for now.
 
+  ;; TODO: figure out if having the CARD_KEY is enough to get the
+  ;; CLEAR_BELL, regardless of in-game story progress.
   (concat pre-tree-items [:BICYCLE
                           :BLUE_CARD
                           :COIN_CASE
                           :SQUIRTBOTTLE]))
 
 (def surf-blocked-items
-  (concat kanto-items [;; surf-blocked because the player can't
-                       :CARD_KEY
-                       ;; trigger the rocket radio tower takeover
-                       :BASEMENT_KEY
-                       ;; without defeating the red gyarados
-                       :CLEAR_BELL
-                       :RED_SCALE
-                       :HM_FLY
-                       :SECRETPOTION
+  (concat kanto-items [:CARD_KEY     ;; surf-blocked because the player can't
+                       :BASEMENT_KEY ;; trigger the rocket radio tower takeover
+                       :CLEAR_BELL   ;; without defeating the red gyarados
+
+                       :RED_SCALE    ;; you have to surf to battle the Gyarados
+
+                       :HM_FLY       ;; on Cianwood
+                       :SECRETPOTION ;; on Cianwood
+
                        ;; surf blocked unless we opt to remove the guy
-                       ;; blocking mahogany town/route 44 junction
+                       ;; blocking mahogany town/route 44
+                       ;; junction. HM_SURF is required to progress
+                       ;; the story, and hence to go to Ice Path.
                        :HM_WATERFALL]))
 
 (def all-items (sort (concat (keys required-items)
