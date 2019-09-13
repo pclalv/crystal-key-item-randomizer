acac0d565134b7b6d62333b43dbc37e5ae916a05
paul alvarez <palvarez@greenhouse.io>
2019-09-12 20:11:36 -0400

    Switch to ring and log req/resp

--- project.clj
+++ project.clj
@@ -5,10 +5,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[compojure "1.6.1"]
-                 [http-kit "2.3.0"]
                  [org.clojure/clojure "1.10.0"]
-                 [reduce-fsm "0.1.4"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-devel "1.7.1"]]
   :main ^:skip-aot crystal-key-item-randomizer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
--- src/crystal_key_item_randomizer/core.clj
+++ src/crystal_key_item_randomizer/core.clj
@@ -1,10 +1,12 @@
 (ns crystal-key-item-randomizer.core
   (:gen-class)
   (:require [clojure.data.json :as json]
-            [org.httpkit.server :as server]
-            [crystal-key-item-randomizer.swaps :as swaps])
+            [crystal-key-item-randomizer.swaps :as swaps]
+            [ring.adapter.jetty :as jetty])
+            ;; [ring.middleware.defaults :refer :all]
   (:use [compojure.route :only [files not-found]]
-        [compojure.core :only [defroutes GET]]))
+        [compojure.core :only [defroutes GET]]
+        [ring.middleware.reload :only [wrap-reload]]))
 
 (defn swaps-handler [_req]
   {:status  200
@@ -17,8 +19,27 @@
   (files "assets")
   (not-found "not found"))
 
+(defn wrap-logger [handler]
+  (fn [request]
+    (let [{:keys [request-method uri]} request
+          {:keys [status body] :as response} (handler request)
+          logline (str ["INFO: -- " {:method (-> request-method name .toUpperCase)
+                                     :uri uri
+                                     :status status
+                                     :response-body body}])]
+      (.println System/out logline)
+      response)))
+
+(def app
+  (->> app-routes
+       (wrap-logger)))
+
+(defn event-logger [event]
+  (println event)
+  (.println System/out event))
+
 (defn -main
   [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
-    (server/run-server #'app-routes {:port port})
+    (jetty/run-jetty #'app {:port port})
     (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
