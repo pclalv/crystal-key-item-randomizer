56853f3512f3fac099ec90f0d86cfda10cbb7c31
paul alvarez <palvarez@greenhouse.io>
2019-09-14 09:44:24 -0400

    Allow for ring development live reloading

--- project.clj
+++ project.clj
@@ -4,16 +4,19 @@
   :url "https://github.com/pclalv/crystal-key-item-randomizer"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[compojure "1.6.1"]
-                 [org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.6"]
                  [ring/ring-core "1.7.1"]
                  [ring/ring-jetty-adapter "1.7.1"]
                  [ring/ring-devel "1.7.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [compojure "1.6.1"]]
   :main ^:skip-aot crystal-key-item-randomizer.core
   :source-paths ["src/clj"]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :production {:env {:production true}}}
-  :uberjar-name "crystal-key-item-randomizer.jar")
+  :uberjar-name "crystal-key-item-randomizer.jar"
+
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler crystal-key-item-randomizer.core/app})
