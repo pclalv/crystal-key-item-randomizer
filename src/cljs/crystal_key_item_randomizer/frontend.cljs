e6c6e34cf96c4f1b7dae14cfd8c53fe6746c3898
paul alvarez <palvarez@greenhouse.io>
2019-09-14 10:52:01 -0400

    Hide input after a file has been selected

--- src/cljs/crystal_key_item_randomizer/frontend.cljs
+++ src/cljs/crystal_key_item_randomizer/frontend.cljs
@@ -1,6 +1,7 @@
 (ns crystal-key-item-randomizer.frontend
   (:require [reagent.core :as r]))
 
+(def input-hidden (r/atom false))
 
 (defn test-component []
   [:a {:href "#test"} "I am a test link created with reagent"])
@@ -12,14 +13,15 @@
 
 (defn handle-rom [event]
   (when (not= "" (-> event .-target .-value))
+    (reset! input-hidden true)
     (let [^js/File file (-> event .-target .-files (aget 0))]
-      ;; seems like the input automatically disappeared? why???
       (embed-download-link))))
 
 (defn rom-input []
-  [:label "Select ROM FILE"
-   [:input {:id "rom-file" :type "file" :accept ".gbc" :on-change handle-rom}]])
+  (if-let [_ (not @input-hidden)]
+    [:label "Select ROM FILE"
+     [:input {:id "rom-file" :type "file" :accept ".gbc" :on-change handle-rom}]]))
 
 (defn init! []
   (r/render [rom-input] (-> js/document
-                            (.getElementById "download"))))
+                            (.getElementById "upload"))))
