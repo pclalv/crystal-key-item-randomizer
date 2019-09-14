8036a2c3f9404cb7b267dd1677098c3b6ed0abe5
paul alvarez <palvarez@greenhouse.io>
2019-09-14 10:30:54 -0400

    Create input in cljs

--- src/cljs/crystal_key_item_randomizer/frontend.cljs
+++ src/cljs/crystal_key_item_randomizer/frontend.cljs
@@ -72,9 +72,13 @@
                                                  (aget 0)))
   (when (not= "" (-> event .-target .-value))
     (let [^js/File file (-> event .-target .-files (aget 0))]
+      ;; seems like the input automatically disappeared? why???
       (embed-download-link))))
 
+(defn rom-input []
+  [:label "Select ROM FILE"
+   [:input {:id "rom-file" :type "file" :accept ".gbc" :on-change handle-rom}]])
+
 (defn init! []
-  (-> js/document
-      (.getElementById "rom-file")
-      (.addEventListener "change" handle-rom false)))
+  (r/render [rom-input] (-> js/document
+                            (.getElementById "download"))))
