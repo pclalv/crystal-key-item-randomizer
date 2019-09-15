cbd7753368d165a0985f28a9e3b5f5e59063d675
paul alvarez <palvarez@greenhouse.io>
2019-09-14 20:49:56 -0400

    Correctly mutate Uint8Array, create download link
    out of necessity, casting to array is hard and not worth it

--- src/cljs/crystal_key_item_randomizer/frontend.cljs
+++ src/cljs/crystal_key_item_randomizer/frontend.cljs
@@ -4,13 +4,23 @@
 
 (def input-hidden (r/atom false))
 
-(defn test-component []
-  [:a {:href "#test"} "I am a test link created with reagent"])
+(defn mismatched-lengths-error [patch]
+  (str "Mismatch between address range, old values and new values for FIXME"))
 
-(defn embed-download-link [filename rom]
+(defn error [_]
+  (js/console.error "ERROR FIXME"))
+
+(defn download-link [href filename]
+  [:a {:href href
+       :download filename}
+   "download randomized ROM"])
+
+(defn embed-download-link [rom filename]
   (let [parent (-> js/document
-                   (.getElementById "download"))]
-    (r/render [test-component] parent)))
+                   (.getElementById "download"))
+        blob (js/Blob. [rom])
+        href (js/window.webkitURL.createObjectURL blob)]
+    (r/render [download-link href filename] parent)))
 
 (defn apply-swap [rom-bytes [original replacement]]
   (let [original-address (-> (keyword original)
@@ -19,20 +29,43 @@
         replacement-value (-> (keyword replacement)
                               key-items
                               :value)]
-    (js/console.log "Replacing " original " with " replacement)
-    (aset rom-bytes original-address replacement-value)))
+    (aset rom-bytes original-address replacement-value)
+    rom-bytes))
 
 (defn apply-swaps [rom-bytes swaps]
   (reduce apply-swap rom-bytes (js->clj swaps)))
 
-(defn apply-diffs [rom-bytes]
-  rom-bytes)
+(defn update-address [rom-bytes {:keys [address old-value new-value]}]
+  (if (not= old-value (aget rom-bytes address))
+    (js/console.log "Error: expected " old-value " at address " address " but found " (aget rom-bytes address))
+    (do (aset rom-bytes address new-value)
+        rom-bytes)))
 
-(defn patch-rom [rom-bytes {:keys [swaps patches]}]
+(defn apply-patch [rom-bytes {{old-values :old new-values :new} :integer_values
+                              {begin-addr :begin end-addr :end} :address_range
+                              name :name
+                              :as patch}]
+  (let [address-range (range begin-addr end-addr)]
+    (if (not= (count address-range) (count old-values) (count new-values))
+      (error (mismatched-lengths-error patch))
+      (let [address-values (map (fn [address old-value new-value]
+                                  {:address address :old-value old-value :new-value new-value})
+                                address-range
+                                old-values
+                                new-values)]
+        (reduce update-address
+                rom-bytes
+                address-values)))))
+
+(defn apply-patches [rom-bytes patches]
+  (reduce apply-patch rom-bytes patches))
+
+(defn patch-rom [rom-bytes swaps patches]
+  ;; TODO: come up with a better name for this fn
   (let [seed "FIXME"]
     (-> rom-bytes
         (apply-swaps swaps)
-        (apply-diffs)
+        (apply-patches patches)
         (embed-download-link (str "pokecrystal-key-item-randomized-seed-" seed ".gbc")))))
 
 (defn randomize-rom [event]
@@ -43,7 +76,10 @@
         (js/fetch)
         (.then (fn [resp] (.json resp))
                (fn [resp] (js/console.error "GET /seed" resp)))
-        (.then (partial patch-rom rom-bytes)
+        (.then (fn [resp]
+                 (patch-rom rom-bytes
+                            (-> resp .-swaps (js->clj :keywordize-keys true))
+                            (-> resp .-patches (js->clj :keywordize-keys true))))
                (fn [resp] (js/console.error "Could not get swaps" resp))))))
 
 (defn handle-rom [event]
