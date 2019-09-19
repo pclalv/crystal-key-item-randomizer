b4c28ee414abc3bf0bb0a7499b7e88650135a975
paul alvarez <palvarez@greenhouse.io>
2019-09-19 18:26:43 -0400

    Generate a patch that changes the contents of the itemball

--- src/clj/crystal_key_item_randomizer/patches.clj
+++ src/clj/crystal_key_item_randomizer/patches.clj
@@ -1,6 +1,9 @@
 (ns crystal-key-item-randomizer.patches
   (:require [clojure.data.json :as json]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io])
+  (:use [crystal-key-item-randomizer.data :only [key-items]]))
+
+(def UNDERGROUND-ITEM-BALL :GoldenrodUndergroundWarehouseUltraBall.ckir_BEFORE_ITEMBALL_ULTRABALL)
 
 (def default
   "Contains data that the frontend can use to modify the ROM file with changes beyond the usual key item swaps."
@@ -9,5 +12,22 @@
        slurp
        json/read-str))
 
-(defn generate [& _]
-  default)
+(defn item-ball [key-item]
+  (let [key-item-value (get-in key-items [key-item :value])]
+    [key-item-value 1])) 
+
+(defn replace-underground-warehouse-ultra-ball-with-key-item [patches {card-key-replacement :CARD_KEY}]
+  (let [{rest :rest patch UNDERGROUND-ITEM-BALL} (->> patches
+                                                      (group-by (fn [patch]
+                                                                  (if (= (name UNDERGROUND-ITEM-BALL)
+                                                                         (:name patch))
+                                                                    (UNDERGROUND-ITEM-BALL)
+                                                                    :rest))))
+        updated-patch (assoc-in patch
+                                ["integer_values" "new"]
+                                (item-ball card-key-replacement))]
+    (conj rest updated-patch)))
+
+(defn generate [swaps]
+  (-> default
+      (replace-underground-warehouse-ultra-ball-with-key-item swaps)))
