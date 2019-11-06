9339024ff0d56b964209c163aa8088e3702f53b1
paul alvarez <palvarez@greenhouse.io>
2019-11-06 09:16:55 -0500

    Write a test

---
+++ test/crystal_key_item_randomizer/patches_test.clj
@@ -0,0 +1,24 @@
+(ns crystal-key-item-randomizer.patches-test
+  (:require [clojure.test :refer :all]
+            [crystal-key-item-randomizer.patches :refer :all]))
+
+(def speedchoice-item-ball
+  (->> speedchoice-patches
+       (filter #(= (% "name")
+                   (name UNDERGROUND-ITEM-BALL-SPEEDCHOICE)))
+       first))
+
+(deftest replace-underground-warehouse-ultra-ball-with-key-item-test
+  (testing "when speedchoice"
+    (is (= {"integer_values" {"old" ["*" 1]
+                              "new" [128 1]}
+            "address_range" {"begin" 514536
+                             "end" 514538}
+            "name" "UndergroundWarehouseUltraBall.ckir_BEFORE_ITEMBALL_ULTRABALL"
+            "description" "Change the contents of the item ball from ULTRA_BALL to CARD_KEY; for real, this needs to be whatever replaces the CARD_KEY. integer_values.new doesn't exist so that things will fail hard if the patches aren't update properly"}
+           (->> (replace-underground-warehouse-ultra-ball-with-key-item speedchoice-patches
+                                                                        {:CARD_KEY :MACHINE_PART}
+                                                                        {:speedchoice? true})
+                (filter #(= (% "name")
+                            (name UNDERGROUND-ITEM-BALL-SPEEDCHOICE)))
+                (first))))))
