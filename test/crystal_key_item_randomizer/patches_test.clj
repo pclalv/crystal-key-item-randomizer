43d426feaebac2de3b4b760eeb07e92e6004c047
paul alvarez <palvarez@greenhouse.io>
2019-11-06 09:21:10 -0500

    Update description

--- test/crystal_key_item_randomizer/patches_test.clj
+++ test/crystal_key_item_randomizer/patches_test.clj
@@ -15,7 +15,7 @@
             "address_range" {"begin" 514536
                              "end" 514538}
             "name" "UndergroundWarehouseUltraBall.ckir_BEFORE_ITEMBALL_ULTRABALL"
-            "description" "Change the contents of the item ball from ULTRA_BALL to CARD_KEY; for real, this needs to be whatever replaces the CARD_KEY. integer_values.new doesn't exist so that things will fail hard if the patches aren't update properly"}
+            "description" "Change the contents of the item ball from ULTRA_BALL to whatever replaces the CARD_KEY (a backup key item so that the player doesn't get softlocked). integer_values.new doesn't exist so that things will fail hard if the patches aren't update properly"}
            (->> (replace-underground-warehouse-ultra-ball-with-key-item speedchoice-patches
                                                                         {:CARD_KEY :MACHINE_PART}
                                                                         {:speedchoice? true})
