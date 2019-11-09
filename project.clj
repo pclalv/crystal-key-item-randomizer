95cf825d2e8fc0bfdf763659aebcd6ddb98cbf95
paul alvarez <palvarez@greenhouse.io>
2019-11-08 22:00:26 -0500

    Update clojure

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :url "https://github.com/pclalv/crystal-key-item-randomizer"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
                  [org.clojure/data.json "0.2.6"]
                  [ring/ring-core "1.7.1"]
