907ae03f50a647b1ed371373fdacf66e27325b95
paul alvarez <palvarez@greenhouse.io>
2019-11-16 10:45:20 -0500

    Don't give S_S_TICKET itself, give whatever it swaps to

--- src/clj/crystal_key_item_randomizer/progression.clj
+++ src/clj/crystal_key_item_randomizer/progression.clj
@@ -232,7 +232,7 @@
                                     (and (conditions-met :can-surf)
                                          (conditions-met :can-strength)
                                          (conditions-met :can-waterfall)))) (-> args
-                                                                                (assoc :items-obtained (conj items-obtained :S_S_TICKET))
+                                                                                (assoc :items-obtained (conj items-obtained (swaps :S_S_TICKET)))
                                                                                 (assoc :conditions-met (conj conditions-met :defeat-elite-4)))
         :else (assoc args :reasons (conj reasons "defeat-elite-4: can't without Victory Road access via Viridian or Tohjo Falls"))))
 
