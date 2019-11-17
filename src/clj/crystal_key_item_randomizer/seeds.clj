331c95455855d172e75ffc61a3c15fa8942bddba
paul alvarez <palvarez@greenhouse.io>
2019-11-16 20:40:23 -0500

    Refactor generate functions
    * generating a particular seed doesn't take into account options.
    * generating a _random_ seed does take into account options.

--- src/clj/crystal_key_item_randomizer/seeds.clj
+++ src/clj/crystal_key_item_randomizer/seeds.clj
@@ -16,28 +16,34 @@
   (let [early-swaps (crystal-key-item-randomizer.progression/get-swaps swaps early-items)]
     (contains? early-swaps item)))
 
-(defn generate-swaps [seed-id {:keys [early-bicycle? early-super-rod?] :as opts}]
-  (let [swaps (zipmap all-items
-                      (deterministic-shuffle all-items seed-id))]
-    (cond (and early-bicycle?   (not (gives-early? :BICYCLE swaps))) (recur seed-id opts)
-          (and early-super-rod? (not (gives-early? :SUPER_ROD swaps))) (recur seed-id opts)
-          :else swaps)))
+(defn generate-swaps [{:keys [early-bicycle? early-super-rod?] :as opts}]
+  (loop [rng (or (:rng opts)
+                 (new java.util.Random))
+         seed-id (-> rng
+                     .nextLong
+                     java.lang.Math/abs)]
+    (let [swaps (zipmap all-items
+                        (deterministic-shuffle all-items seed-id))]
+      (cond (and early-bicycle?   (not (gives-early? :BICYCLE swaps))) (recur)
+            (and early-super-rod? (not (gives-early? :SUPER_ROD swaps))) (recur)
+            :else swaps))))
 
-(defn generate [seed-id {:keys [early-bicycle? early-super-rod?] :as options}]
-  (let [swaps (generate-swaps seed-id options)
+(defn generate-random [options]
+  (loop []
+    (let [swaps (generate-swaps options)
+          progression-results (beatable? swaps {:speedchoice? true})]
+      (if (progression-results :beatable?)
+        {:seed (-> progression-results
+                   (assoc :patches (patches/generate swaps {:speedchoice? true}))
+                   (assoc :id (str seed-id)))}
+        (recur)))))
+
+(defn generate [seed-id]
+  (let [swaps (zipmap all-items
+                      (deterministic-shuffle all-items seed-id))
         progression-results (beatable? swaps {:speedchoice? true})]
     (if (progression-results :beatable?)
       {:seed (-> progression-results
                  (assoc :patches (patches/generate swaps {:speedchoice? true}))
                  (assoc :id (str seed-id)))}
       {:error (str "Unbeatable seed: " seed-id)})))
-
-(defn generate-beatable [opts]
-  (loop []
-    (let [seed-id (-> (new java.util.Random)
-                      .nextLong
-                      java.lang.Math/abs)
-          {:keys [seed error]} (generate seed-id opts)]
-      (if error
-        (recur)
-        {:seed seed}))))
