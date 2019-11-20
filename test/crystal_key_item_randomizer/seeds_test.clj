1da0a8fa41720e44dcb6fec77b21e99c3912e4c0
paul alvarez <palvarez@greenhouse.io>
2019-11-19 21:10:32 -0500

    Fix tests

--- test/crystal_key_item_randomizer/seeds_test.clj
+++ test/crystal_key_item_randomizer/seeds_test.clj
@@ -13,67 +13,67 @@
 
 (deftest generate-random-test
   (testing "when early-bicycle? is true"
-    (is (= {:swaps          
-            {:HM_FLASH :HM_WATERFALL,
-             :SILVER_WING :SECRETPOTION,
-             :LOST_ITEM :S_S_TICKET,
-             :ITEMFINDER :BASEMENT_KEY,
-             :GOOD_ROD :COIN_CASE,
-             :CARD_KEY :SILVER_WING,
-             :COIN_CASE :BICYCLE,
-             :BLUE_CARD :MYSTERY_EGG,
-             :CLEAR_BELL :HM_WHIRLPOOL,
-             :SQUIRTBOTTLE :BLUE_CARD,
-             :HM_WHIRLPOOL :GOOD_ROD,
-             :RED_SCALE :HM_FLY,
-             :HM_WATERFALL :CLEAR_BELL,
-             :SECRETPOTION :SUPER_ROD,
-             :BASEMENT_KEY :HM_FLASH,
-             :MACHINE_PART :OLD_ROD,
-             :MYSTERY_EGG :RED_SCALE,
-             :S_S_TICKET :LOST_ITEM,
-             :PASS :CARD_KEY,
-             :HM_CUT :PASS,
-             :HM_FLY :HM_CUT,
-             :HM_STRENGTH :HM_STRENGTH,
-             :OLD_ROD :HM_SURF,
-             :BICYCLE :SQUIRTBOTTLE,
-             :HM_SURF :MACHINE_PART,
-             :SUPER_ROD :ITEMFINDER},
-            :id "471763713"} (-> (generate-random {:early-bicycle? true
-                                                   :rng (new java.util.Random 1)})
-                                 :seed
-                                 (select-keys [:swaps :id])))))
+    (is (= {:id "155629808"
+            :swaps {:HM_FLASH :RED_SCALE
+                    :SILVER_WING :HM_FLY
+                    :LOST_ITEM :ITEMFINDER
+                    :ITEMFINDER :S_S_TICKET
+                    :GOOD_ROD :GOOD_ROD
+                    :CARD_KEY :HM_WHIRLPOOL
+                    :COIN_CASE :SQUIRTBOTTLE
+                    :BLUE_CARD :HM_WATERFALL
+                    :CLEAR_BELL :HM_FLASH
+                    :SQUIRTBOTTLE :HM_STRENGTH
+                    :HM_WHIRLPOOL :PASS
+                    :RED_SCALE :MACHINE_PART
+                    :HM_WATERFALL :LOST_ITEM
+                    :SECRETPOTION :OLD_ROD
+                    :BASEMENT_KEY :SECRETPOTION
+                    :MACHINE_PART :SILVER_WING
+                    :MYSTERY_EGG :BLUE_CARD
+                    :S_S_TICKET :COIN_CASE
+                    :PASS :CARD_KEY
+                    :HM_CUT :SUPER_ROD
+                    :HM_FLY :CLEAR_BELL
+                    :HM_STRENGTH :HM_CUT
+                    :OLD_ROD :BICYCLE
+                    :BICYCLE :MYSTERY_EGG
+                    :HM_SURF :BASEMENT_KEY
+                    :SUPER_ROD :HM_SURF}}
+           (-> (generate-random {:early-bicycle? true
+                                 :rng (new java.util.Random 1)})
+               :seed
+               (select-keys [:swaps :id])))))
 
   (testing "when no-early-super-rod? is false"
-    (is (= {:swaps          
-            {:HM_FLASH :MYSTERY_EGG,
-             :SILVER_WING :MACHINE_PART,
-             :LOST_ITEM :PASS,
-             :ITEMFINDER :OLD_ROD,
-             :GOOD_ROD :CARD_KEY,
-             :CARD_KEY :HM_CUT,
-             :COIN_CASE :HM_FLASH,
-             :BLUE_CARD :HM_SURF,
-             :CLEAR_BELL :SILVER_WING,
-             :SQUIRTBOTTLE :SQUIRTBOTTLE,
-             :HM_WHIRLPOOL :BICYCLE,
-             :RED_SCALE :HM_STRENGTH,
-             :HM_WATERFALL :COIN_CASE,
-             :SECRETPOTION :ITEMFINDER,
-             :BASEMENT_KEY :BLUE_CARD,
-             :MACHINE_PART :CLEAR_BELL,
-             :MYSTERY_EGG :HM_WHIRLPOOL,
-             :S_S_TICKET :RED_SCALE,
-             :PASS :GOOD_ROD,
-             :HM_CUT :SECRETPOTION,
-             :HM_FLY :SUPER_ROD,
-             :HM_STRENGTH :HM_FLY,
-             :OLD_ROD :BASEMENT_KEY,
-             :BICYCLE :LOST_ITEM,
-             :HM_SURF :S_S_TICKET,
-             :SUPER_ROD :HM_WATERFALL},
-            :id "1559630936"} (-> (generate-random {:no-early-super-rod? true
-                                                    :rng (new java.util.Random 1)})
-                                  :seed
-                                  (select-keys [:swaps :id]))))))
+    (is (= {:id "1749940626"
+            :swaps {:HM_FLASH :COIN_CASE
+                    :SILVER_WING :CARD_KEY
+                    :LOST_ITEM :HM_CUT
+                    :ITEMFINDER :MYSTERY_EGG
+                    :GOOD_ROD :SILVER_WING
+                    :CARD_KEY :HM_FLY
+                    :COIN_CASE :HM_FLASH
+                    :BLUE_CARD :PASS
+                    :CLEAR_BELL :SUPER_ROD
+                    :SQUIRTBOTTLE :SQUIRTBOTTLE
+                    :HM_WHIRLPOOL :OLD_ROD
+                    :RED_SCALE :GOOD_ROD
+                    :HM_WATERFALL :SECRETPOTION
+                    :SECRETPOTION :LOST_ITEM
+                    :BASEMENT_KEY :ITEMFINDER
+                    :MACHINE_PART :MACHINE_PART
+                    :MYSTERY_EGG :HM_WHIRLPOOL
+                    :S_S_TICKET :BLUE_CARD
+                    :PASS :S_S_TICKET
+                    :HM_CUT :HM_STRENGTH
+                    :HM_FLY :RED_SCALE
+                    :HM_STRENGTH :HM_WATERFALL
+                    :OLD_ROD :HM_SURF
+                    :BICYCLE :BASEMENT_KEY
+                    :HM_SURF :BICYCLE
+                    :SUPER_ROD :CLEAR_BELL}}
+           (-> (generate-random {:no-early-super-rod? true
+                                 :rng (new java.util.Random 1)})
+               :seed
+               (select-keys [:swaps :id]))))))
