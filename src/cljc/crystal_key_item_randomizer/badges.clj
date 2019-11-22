b18d4a18cc3a237b27169fdde3698182a67b8a76
paul alvarez <palvarez@greenhouse.io>
2019-11-21 21:38:51 -0500

    Define badges namespace
    you know what's next

---
+++ src/cljc/crystal_key_item_randomizer/badges.clj
@@ -0,0 +1,18 @@
+(ns crystal-key-item-randomizer.badges)
+
+(def speedchoice {:PLAINBADGE {:address 344157 :value 29}
+                  :MARSHBADGE {:address 1612913 :value 40}
+                  :RISINGBADGE {:address 1658470 :value 34}
+                  :FOGBADGE {:address 630139 :value 30}
+                  :ZEPHYRBADGE {:address 426992 :value 27}
+                  :RAINBOWBADGE {:address 470232 :value 38}
+                  :STORMBADGE {:address 644690 :value 32}
+                  :VOLCANOBADGE {:address 1750307 :value 41}
+                  :SOULBADGE {:address 1662993 :value 39}
+                  :EARTHBADGE {:address 633417 :value 42}
+                  :THUNDERBADGE {:address 1646809 :value 37}
+                  :HIVEBADGE {:address 1633200 :value 28}
+                  :MINERALBADGE {:address 639314 :value 31}
+                  :BOULDERBADGE {:address 1714314 :value 35}
+                  :CASCADEBADGE {:address 1606750 :value 36}
+                  :GLACIERBADGE {:address 1678273 :value 33}})
