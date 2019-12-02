e558c39558fc6da099ef608bbed60a8e28d892c3
paul alvarez <palvarez@greenhouse.io>
2019-12-01 23:25:18 -0500

    Move itemball patch into code

--- src/clj/crystal_key_item_randomizer/patches.clj
+++ src/clj/crystal_key_item_randomizer/patches.clj
@@ -7,6 +7,14 @@
 (def UNDERGROUND-ITEM-BALL :GoldenrodUndergroundWarehouseUltraBall.ckir_BEFORE_ITEMBALL_ULTRABALL)
 (def UNDERGROUND-ITEM-BALL-SPEEDCHOICE :UndergroundWarehouseUltraBall.ckir_BEFORE_ITEMBALL_ULTRABALL)
 
+
+(def underground-warehouse-ultra-ball
+  {:label "UndergroundWarehouseUltraBall.ckir_BEFORE_ITEMBALL_ULTRABALL"
+   :description "Change the contents of the item ball from ULTRA_BALL to whatever replaces the CARD_KEY (a backup key item so that the player doesn't get softlocked). integer_values.new doesn't exist so that things will fail hard if the patches aren't update properly"
+   :integer_values {:old ["*", 1]}
+   :address_range {:begin 514536
+                   :end 514538}})
+
 (def standalone-checkflag-badges
   {:PLAINBADGE {:label "WhitneyScript_0x5400c.ckir_BEFORE_checkflag_ENGINE_PLAINBADGE"
                 :description "Check whatever badge is actually given by Whitney, even in badge rando"
@@ -42,19 +50,10 @@
     [key-item-value 1]))
 
 (defn replace-underground-warehouse-ultra-ball-with-key-item [patches {card-key-replacement :CARD_KEY} {:keys [speedchoice?]}]
-  (let [{rest :rest patch UNDERGROUND-ITEM-BALL} (->> patches
-                                                      (group-by (fn [patch]
-                                                                  (if (= (name (if speedchoice?
-                                                                                 UNDERGROUND-ITEM-BALL-SPEEDCHOICE
-                                                                                 UNDERGROUND-ITEM-BALL))
-                                                                         (patch "label"))
-                                                                    UNDERGROUND-ITEM-BALL
-                                                                    :rest))))
-        patch (first patch)
-        updated-patch (assoc-in patch
-                                ["integer_values" "new"]
-                                (item-ball card-key-replacement {:speedchoice? speedchoice?}))]
-    (conj rest updated-patch)))
+  (let [patch (assoc-in underground-warehouse-ultra-ball
+                        [:integer_values :new]
+                        (item-ball card-key-replacement {:speedchoice? speedchoice?}))]
+    (conj patches patch)))
 
 (defn checkflag [badge]
   (let [badge-flag-value (-> badges/speedchoice badge :value)]
--- test/crystal_key_item_randomizer/patches_test.clj
+++ test/crystal_key_item_randomizer/patches_test.clj
@@ -10,16 +10,16 @@
 
 (deftest replace-underground-warehouse-ultra-ball-with-key-item-test
   (testing "when speedchoice"
-    (is (= {"integer_values" {"old" ["*" 1]
-                              "new" [128 1]}
-            "address_range" {"begin" 514536
-                             "end" 514538}
-            "label" "UndergroundWarehouseUltraBall.ckir_BEFORE_ITEMBALL_ULTRABALL"
-            "description" "Change the contents of the item ball from ULTRA_BALL to whatever replaces the CARD_KEY (a backup key item so that the player doesn't get softlocked). integer_values.new doesn't exist so that things will fail hard if the patches aren't update properly"}
+    (is (= {:integer_values {:old ["*" 1]
+                             :new [128 1]}
+            :address_range {:begin 514536
+                            :end 514538}
+            :label "UndergroundWarehouseUltraBall.ckir_BEFORE_ITEMBALL_ULTRABALL"
+            :description "Change the contents of the item ball from ULTRA_BALL to whatever replaces the CARD_KEY (a backup key item so that the player doesn't get softlocked). integer_values.new doesn't exist so that things will fail hard if the patches aren't update properly"}
            (->> (replace-underground-warehouse-ultra-ball-with-key-item speedchoice-patches
                                                                         {:CARD_KEY :MACHINE_PART}
                                                                         {:speedchoice? true})
-                (filter #(= (% "label")
+                (filter #(= (% :label)
                             (name UNDERGROUND-ITEM-BALL-SPEEDCHOICE)))
                 (first))))))
 
