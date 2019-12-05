d96e47ef3d170687c54768d8b5da706edea1f64f
paul alvarez <palvarez@greenhouse.io>
2019-12-05 11:10:13 -0500

    Improve text

--- src/clj/crystal_key_item_randomizer/patches/badges.clj
+++ src/clj/crystal_key_item_randomizer/patches/badges.clj
@@ -21,16 +21,16 @@
   "For badge randomization, we patch out most `checkflag*BADGE` calls,
   for example, those in the Kanto gyms that essentially serve to check
   whether the player has beaten the Gym Leader yet or not. If we were
-  to leave those checks alone, then the reuslt of badge randomization
+  to leave those checks alone, then the result of badge randomization
   would be that obtaining the THUNDERBADGE from Bugsy would mean that
-  the player could not even battle Lt Surge, let alone get his
-  badge. Those checks are easily replaced with `checkevent` calls to
-  see if the player has actually beaten the gym leader or not.
+  the player could not battle Lt Surge, let alone get his badge. Those
+  checks are easily replaced with `checkevent` calls to see if the
+  player has actually beaten the gym leader or not.
 
   There are two additional problemation `checkflag` calls around
   interactions with Whitney and Clair in their respective gyms, owing
   to the fact that the vanilla game does not immediately grant either
-  badge. Instead,we need to acutally check if the player has received
+  badge. Instead, we need to acutally check if the player has received
   the replacement badge."
   [patches original-badge badge-swaps]
   (let [replacement-badge (badge-swaps original-badge)
