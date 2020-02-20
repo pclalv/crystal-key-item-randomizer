6d5a6ec4e67b12c11fc5345e077f38f0ee685278
paul alvarez <palvarez@greenhouse.io>
2020-02-20 09:08:34 -0500

    Write my very first CLJS test

--- test/cljs/crystal_key_item_randomizer/frontend_test.cljs
+++ test/cljs/crystal_key_item_randomizer/frontend_test.cljs
@@ -1,6 +1,12 @@
 (ns crystal-key-item-randomizer.frontend-test
-  (:require [cljs.test :refer (deftest is)]))
+  (:require [cljs.test :refer (deftest testing is)]
+            [crystal-key-item-randomizer.frontend :as frontend])
+  (:require-macros [crystal-key-item-randomizer.test-utils :as test-utils]))
 
-(deftest a-failing-test
-  (is (= 1 2)))
+(def rom (test-utils/slurp-bytes-at-compile-time "test/fixtures/crystal-speedchoice-v6.gbc"))
 
+(deftest patch-rom-test
+  (testing "idempotency"
+    (is (= rom (frontend/patch-rom rom {:item-swaps {}
+                                        :badge-swaps {}
+                                        :patches []})))))
---
+++ test/cljs/crystal_key_item_randomizer/test_utils.clj
@@ -0,0 +1,9 @@
+(ns crystal-key-item-randomizer.test-utils)
+
+(defmacro slurp-bytes-at-compile-time
+  "Slurp the bytes from a slurpable thing"
+  [f]
+  (clojure.core/with-open [out (java.io.ByteArrayOutputStream.)]
+    (clojure.java.io/copy (clojure.java.io/input-stream f) out)
+    (->> out .toByteArray
+         (mapv #(java.lang.Byte/toUnsignedInt %)))))
