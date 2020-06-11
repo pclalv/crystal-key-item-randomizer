83d447051486a60e76beabdf0c4a017f1c4014f0
paul alvarez <pclalv@users.noreply.github.com>
2020-06-11 12:30:09 -0400

    Format

--- test/clj/crystal_key_item_randomizer/seeds_test.clj
+++ test/clj/crystal_key_item_randomizer/seeds_test.clj
@@ -20,33 +20,32 @@
 
   (testing "early-bicycle?"
     (testing "when false"
-      (is (= {:item-swaps          
-              {:HM_FLASH :HM_STRENGTH,
-               :SILVER_WING :S_S_TICKET,
-               :LOST_ITEM :HM_WATERFALL,
-               :ITEMFINDER :SECRETPOTION,
-               :GOOD_ROD :CLEAR_BELL,
-               :CARD_KEY :BICYCLE,
-               :COIN_CASE :COIN_CASE,
-               :BLUE_CARD :HM_SURF,
-               :CLEAR_BELL :HM_FLY,
-               :SQUIRTBOTTLE :GOOD_ROD,
-               :HM_WHIRLPOOL :BLUE_CARD,
-               :RED_SCALE :HM_FLASH,
-               :HM_WATERFALL :MACHINE_PART,
-               :SECRETPOTION :HM_CUT,
-               :BASEMENT_KEY :SUPER_ROD,
-               :MACHINE_PART :BASEMENT_KEY,
-               :MYSTERY_EGG :OLD_ROD,
-               :S_S_TICKET :LOST_ITEM,
-               :PASS :SILVER_WING,
-               :HM_CUT :ITEMFINDER,
-               :HM_FLY :MYSTERY_EGG,
-               :HM_STRENGTH :PASS,
-               :OLD_ROD :SQUIRTBOTTLE,
-               :BICYCLE :HM_WHIRLPOOL,
-               :HM_SURF :RED_SCALE,
-               :SUPER_ROD :CARD_KEY},
+      (is (= {:item-swaps {:HM_FLASH :HM_STRENGTH,
+                           :SILVER_WING :S_S_TICKET,
+                           :LOST_ITEM :HM_WATERFALL,
+                           :ITEMFINDER :SECRETPOTION,
+                           :GOOD_ROD :CLEAR_BELL,
+                           :CARD_KEY :BICYCLE,
+                           :COIN_CASE :COIN_CASE,
+                           :BLUE_CARD :HM_SURF,
+                           :CLEAR_BELL :HM_FLY,
+                           :SQUIRTBOTTLE :GOOD_ROD,
+                           :HM_WHIRLPOOL :BLUE_CARD,
+                           :RED_SCALE :HM_FLASH,
+                           :HM_WATERFALL :MACHINE_PART,
+                           :SECRETPOTION :HM_CUT,
+                           :BASEMENT_KEY :SUPER_ROD,
+                           :MACHINE_PART :BASEMENT_KEY,
+                           :MYSTERY_EGG :OLD_ROD,
+                           :S_S_TICKET :LOST_ITEM,
+                           :PASS :SILVER_WING,
+                           :HM_CUT :ITEMFINDER,
+                           :HM_FLY :MYSTERY_EGG,
+                           :HM_STRENGTH :PASS,
+                           :OLD_ROD :SQUIRTBOTTLE,
+                           :BICYCLE :HM_WHIRLPOOL,
+                           :HM_SURF :RED_SCALE,
+                           :SUPER_ROD :CARD_KEY},
               :id "155629808"}
              (-> (generate-random {:swaps-options {:early-bicycle? false
                                                    :rng (new java.util.Random 1)}
@@ -210,23 +209,22 @@
                  :seed
                  (select-keys [:badge-swaps :id])))))
     (testing "when false"
-      (is (= {:badge-swaps
-              {:PLAINBADGE :PLAINBADGE,
-               :MARSHBADGE :MARSHBADGE,
-               :RISINGBADGE :RISINGBADGE,
-               :FOGBADGE :FOGBADGE,
-               :ZEPHYRBADGE :ZEPHYRBADGE,
-               :RAINBOWBADGE :RAINBOWBADGE,
-               :STORMBADGE :STORMBADGE,
-               :VOLCANOBADGE :VOLCANOBADGE,
-               :SOULBADGE :SOULBADGE,
-               :EARTHBADGE :EARTHBADGE,
-               :THUNDERBADGE :THUNDERBADGE,
-               :HIVEBADGE :HIVEBADGE,
-               :MINERALBADGE :MINERALBADGE,
-               :BOULDERBADGE :BOULDERBADGE,
-               :CASCADEBADGE :CASCADEBADGE,
-               :GLACIERBADGE :GLACIERBADGE}
+      (is (= {:badge-swaps {:PLAINBADGE :PLAINBADGE,
+                            :MARSHBADGE :MARSHBADGE,
+                            :RISINGBADGE :RISINGBADGE,
+                            :FOGBADGE :FOGBADGE,
+                            :ZEPHYRBADGE :ZEPHYRBADGE,
+                            :RAINBOWBADGE :RAINBOWBADGE,
+                            :STORMBADGE :STORMBADGE,
+                            :VOLCANOBADGE :VOLCANOBADGE,
+                            :SOULBADGE :SOULBADGE,
+                            :EARTHBADGE :EARTHBADGE,
+                            :THUNDERBADGE :THUNDERBADGE,
+                            :HIVEBADGE :HIVEBADGE,
+                            :MINERALBADGE :MINERALBADGE,
+                            :BOULDERBADGE :BOULDERBADGE,
+                            :CASCADEBADGE :CASCADEBADGE,
+                            :GLACIERBADGE :GLACIERBADGE}
               :id "155629808"}
              (-> (generate-random {:swaps-options {:randomize-badges? false
                                                    :rng (new java.util.Random 1)}
@@ -238,29 +236,32 @@
 
   (testing "randomize-copycat-item?"
     (testing "when true"
-      (is (= {:copycat-item :ITEMFINDER, :id "155629808"} (-> (generate-random {:swaps-options {:randomize-copycat-item? true
-                                                                                                :rng (new java.util.Random 1)}
-                                                                                :logic-options {:endgame-condition :defeat-red
-                                                                                                :rockets :normal
-                                                                                                :speedchoice? true}})
-                                                              :seed
-                                                              (select-keys [:copycat-item :id])))))
+      (is (= {:copycat-item :ITEMFINDER,
+              :id "155629808"}
+             (-> (generate-random {:swaps-options {:randomize-copycat-item? true
+                                                   :rng (new java.util.Random 1)}
+                                   :logic-options {:endgame-condition :defeat-red
+                                                   :rockets :normal
+                                                   :speedchoice? true}})
+                 :seed
+                 (select-keys [:copycat-item :id])))))
     (testing "when false"
-      (is (= {:copycat-item :LOST_ITEM :id "155629808"} (-> (generate-random {:swaps-options {:randomize-copycat-item? false
-                                                                                              :rng (new java.util.Random 1)}
-                                                                              :logic-options {:endgame-condition :defeat-red
-                                                                                              :rockets :normal
-                                                                                              :speedchoice? true}})
-                                                            :seed
-                                                            (select-keys [:copycat-item :id]))))))
+      (is (= {:copycat-item :LOST_ITEM
+              :id "155629808"}
+             (-> (generate-random {:swaps-options {:randomize-copycat-item? false
+                                                   :rng (new java.util.Random 1)}
+                                   :logic-options {:endgame-condition :defeat-red
+                                                   :rockets :normal
+                                                   :speedchoice? true}})
+                 :seed
+                 (select-keys [:copycat-item :id]))))))
 
   (testing "rockets"
     (testing "when normal"
-      (is (= {:item-swaps
-              {:CARD_KEY :BICYCLE,
-               :CLEAR_BELL :HM_FLY,
-               :HM_WHIRLPOOL :BLUE_CARD,
-               :BASEMENT_KEY :SUPER_ROD},
+      (is (= {:item-swaps {:CARD_KEY :BICYCLE,
+                           :CLEAR_BELL :HM_FLY,
+                           :HM_WHIRLPOOL :BLUE_CARD,
+                           :BASEMENT_KEY :SUPER_ROD},
               :id "155629808"}
              (-> (generate-random {:swaps-options {:rng (new java.util.Random 1)}
                                    :logic-options {:rockets :normal
@@ -270,11 +271,10 @@
                  (select-keys [:item-swaps :id])
                  (update :item-swaps select-keys lance-items)))))
     (testing "when early"
-      (is (= {:item-swaps
-              {:CARD_KEY :BICYCLE,
-               :CLEAR_BELL :HM_FLY,
-               :HM_WHIRLPOOL :BLUE_CARD,
-               :BASEMENT_KEY :SUPER_ROD},
+      (is (= {:item-swaps {:CARD_KEY :BICYCLE,
+                           :CLEAR_BELL :HM_FLY,
+                           :HM_WHIRLPOOL :BLUE_CARD,
+                           :BASEMENT_KEY :SUPER_ROD},
               :id "155629808"}
              (-> (generate-random {:swaps-options {:rng (new java.util.Random 1)}
                                    :logic-options {:rockets :early
@@ -284,11 +284,10 @@
                  (select-keys [:item-swaps :id])
                  (update :item-swaps select-keys lance-items)))))
     (testing "when rocketless"
-      (is (= {:item-swaps          
-              {:CARD_KEY :MYSTERY_EGG,
-               :CLEAR_BELL :CARD_KEY,
-               :HM_WHIRLPOOL :ITEMFINDER,
-               :BASEMENT_KEY :BLUE_CARD},
+      (is (= {:item-swaps {:CARD_KEY :MYSTERY_EGG,
+                           :CLEAR_BELL :CARD_KEY,
+                           :HM_WHIRLPOOL :ITEMFINDER,
+                           :BASEMENT_KEY :BLUE_CARD},
               :id "587682406"}
              (-> (generate-random {:swaps-options {:rng (new java.util.Random 1)}
                                    :logic-options {:rockets :rocketless
