de5497a7368c01b6bfa3fcd4285183a3bd2ce81e
paul alvarez <pclalv@users.noreply.github.com>
2020-06-13 22:45:49 -0400

    Read copycat patches from json

--- src/clj/crystal_key_item_randomizer/patches/copycat.clj
+++ src/clj/crystal_key_item_randomizer/patches/copycat.clj
@@ -1,26 +1,18 @@
 (ns crystal-key-item-randomizer.patches.copycat
-  (:use [crystal-key-item-randomizer.text-encoding :only [gsc-encode-to-original-length]]))
+  (:use [crystal-key-item-randomizer.text-encoding :only [gsc-encode-to-original-length]]
+        [crystal-key-item-randomizer.patches.data :only [get-patch]]))
 
 (def lost-your-favorite-doll-replacement-text-template
   "\\plost my\\n{{item}}.")
 
 (def lost-your-favorite-doll-text-patch
-  {:label "UnknownText_0x18b064.ckir_BEFORE_para_lost_your_favorite_doll",
-   :address_range {:begin 1618094,
-                   :end 1618121},
-   :integer_values {:old [81 171 174 178 179 127 184 174 180 177 127 165 160 181 174 177 168 179 164 79 84 127 131 142 139 139 232]}})
+  (get-patch "UnknownText_0x18b064.ckir_BEFORE_para_lost_your_favorite_doll"))
 
 (def take-item-lost-item-patch
-  {:label "Copycat.ckir_BEFORE_takeitem_LOST_ITEM"
-   :integer_values {:old [32 130 1]}
-   :address_range {:begin 1617793
-                   :end 1617796}})
+  (get-patch "Copycat.ckir_BEFORE_takeitem_LOST_ITEM"))
 
 (def check-item-lost-item-patch
-  {:label "Copycat.ckir_BEFORE_checkitem_LOST_ITEM"
-   :integer_values {:old [33 130]}
-   :address_range {:begin 1617643
-                   :end 1617645}})
+  (get-patch "Copycat.ckir_BEFORE_checkitem_LOST_ITEM"))
 
 (defn fix-text [copycat-item]
   (let [item-name (-> copycat-item
